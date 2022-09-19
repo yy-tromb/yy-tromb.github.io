d2bc98097959cafd4b2d0af9ac8a1d4ba4226129
yyhome-tromb <107352731+yyhome-tromb@users.noreply.github.com>
2022-09-19 11:10:39 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let pageCaches={};
+let gas=google.script;
+
+const openMenuBtn=document.getElementById("openMenuBtn");
+const openMenuBtn_span=openMenuBtn.getElementsByTagName("span")[0];
+const navEle=document.getElementsByTagName("nav")[0];
+
+openMenuBtn.addEventListener("click",e=>{
+  if(navEle.style.marginRight==="-30vw"){
+    navEle.style.marginRight="0";
+  }else{
+    navEle.style.marginRight="-30vw";
+  }
+  
+});
+openMenuBtn.onmouseover=()=>openMenuBtn_span.style.backgroundColor="rgb(100,100,100)"
+openMenuBtn.onmouseout=e=>openMenuBtn_span.style.backgroundColor="var(--theme-color)";
